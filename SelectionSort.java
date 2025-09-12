092b5c947a29a9074182af28b6ed81f715790353
Jony <jony378892@gmail.com>
2025-09-13 01:57:28 +0600

    Selection Sort using swap

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,4 +1,4 @@
-public class SelectionSort {
+/* public class SelectionSort {
     public static void main(String[] args) {
         int[] my_array = { 64, 34, 25, 5, 22, 11, 90, 12 };
         int n = my_array.length;
@@ -23,6 +23,29 @@ public class SelectionSort {
         }
         System.out.println();
     }
-}
+} */
 
-// Java
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] my_array = { 64, 34, 25, 12, 22, 11, 90, 5 };
+        int n = my_array.length;
+
+        for (int i = 0; i < n; i++) {
+            int min_index = i;
+            for (int j = i + 1; j < n; j++) {
+                if (my_array[j] < my_array[min_index]) {
+                    min_index = j;
+                }
+            }
+            int temp = my_array[i];
+            my_array[i] = my_array[min_index];
+            my_array[min_index] = temp;
+        }
+
+        System.out.print("Sorted array: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(my_array[i] + " ");
+        }
+        System.out.println();
+    }
+}
